92278176d819f55c3e2065853a363189fda60de6
Tim Karpov <tim@Tims-MacBook-Pro.local>
2017-05-03 07:10:52 -0500

    learnhaskell

---
+++ my-project1/src/Lib.hs
@@ -0,0 +1,21 @@
+module Lib
+    (
+Main = do  
+putStrinLn "Hello, what is your name?"
+name <- getLine
+ putStrLn <- getLine
+putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
+
+import Data.Char
+
+Main = do
+putStrLn "What is your name?"
+line <- getLine
+if ("Dmitry Tadyshev" line
+then return "Hi, Dmitry") 
+else (do 
+putStrLn $ "Intruder, INTRUDER!!! Sound the alarms!")
+   ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
